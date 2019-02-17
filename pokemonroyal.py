bc8997c774352d569aa8f6fce6d30b3d4c74e25a
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-17 11:30:13 +0600

    Update pokemonroyal.py

--- pokemonroyal.py
+++ pokemonroyal.py
@@ -48,8 +48,6 @@ def is_owner(ctx):
 
 @client.event
 async def on_member_join(member):
-    role = discord.utils.get(member.server.roles, name='member')
-    await client.add_roles(member, role)
     print("In our server" + member.name + " just joined")
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
