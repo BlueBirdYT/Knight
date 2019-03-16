149cd0d9d30079c41d3c122faebfa4bdc4dd1675
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-03-16 14:04:31 +0600

    Update pokemonroyal.py

--- pokemonroyal.py
+++ pokemonroyal.py
@@ -1324,7 +1324,15 @@ def get_xp(user_id: int):
     else:
         return 0            
         
-        
+@client.command(pass_context=True)
+@commands.check(is_owner)
+async def setgame(ctx,types:str,*,game:str):
+    rep = {"listening": "2", "watching": "3", "streaming": "1", "playing": "0"} # define desired replacements here
+    rep = dict((re.escape(k), v) for k, v in rep.iteritems())
+    pattern = re.compile("|".join(rep.keys()))
+    text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+    await client.delete_message(ctx.message)
+    await client.change_presence(game=discord.Game(name=game, type=int(text)))        
         
         
         
